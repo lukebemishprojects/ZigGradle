da23e916eb2f7f6f1097896bd1fa19686b252cbe
FalsePattern <me@falsepattern.com>
2025-05-03 16:29:17 +0200

    add all architecture and OS targets from zig 0.14.0

--- src/main/java/dev/lukebemish/ziggradle/ZigArchitectureTarget.java
+++ src/main/java/dev/lukebemish/ziggradle/ZigArchitectureTarget.java
@@ -6,7 +6,55 @@ import org.gradle.api.Named;
 import java.io.Serializable;
 
 public interface ZigArchitectureTarget extends Named, Serializable {
-    ZigArchitectureTarget X86_64 = new DefaultZigArchitectureTarget("x86_64");
-    ZigArchitectureTarget AARCH64 = new DefaultZigArchitectureTarget("aarch64");
-    ZigArchitectureTarget X86 = new DefaultZigArchitectureTarget("x86");
+    // @formatter:off
+    ZigArchitectureTarget AMDGCN      = of("amdgcn"     );
+    ZigArchitectureTarget ARC         = of("arc"        );
+    ZigArchitectureTarget ARM         = of("arm"        );
+    ZigArchitectureTarget ARMEB       = of("armeb"      );
+    ZigArchitectureTarget THUMB       = of("thumb"      );
+    ZigArchitectureTarget THUMBEB     = of("thumbeb"    );
+    ZigArchitectureTarget AARCH64     = of("aarch64"    );
+    ZigArchitectureTarget AARCH64_BE  = of("aarch64_be" );
+    ZigArchitectureTarget AVR         = of("avr"        );
+    ZigArchitectureTarget BPFEL       = of("bpfel"      );
+    ZigArchitectureTarget BPFEB       = of("bpfeb"      );
+    ZigArchitectureTarget CSKY        = of("csky"       );
+    ZigArchitectureTarget HEXAGON     = of("hexagon"    );
+    ZigArchitectureTarget KALIMBA     = of("kalimba"    );
+    ZigArchitectureTarget LANAI       = of("lanai"      );
+    ZigArchitectureTarget LOONGARCH32 = of("loongarch32");
+    ZigArchitectureTarget LOONGARCH64 = of("loongarch64");
+    ZigArchitectureTarget M68K        = of("m68k"       );
+    ZigArchitectureTarget MIPS        = of("mips"       );
+    ZigArchitectureTarget MIPSEL      = of("mipsel"     );
+    ZigArchitectureTarget MIPS64      = of("mips64"     );
+    ZigArchitectureTarget MIPS64EL    = of("mips64el"   );
+    ZigArchitectureTarget MSP430      = of("msp430"     );
+    ZigArchitectureTarget NVPTX       = of("nvptx"      );
+    ZigArchitectureTarget NVPTX64     = of("nvptx64"    );
+    ZigArchitectureTarget POWERPC     = of("powerpc"    );
+    ZigArchitectureTarget POWERPCLE   = of("powerpcle"  );
+    ZigArchitectureTarget POWERPC64   = of("powerpc64"  );
+    ZigArchitectureTarget POWERPC64LE = of("powerpc64le");
+    ZigArchitectureTarget PROPELLER   = of("propeller"  );
+    ZigArchitectureTarget RISCV32     = of("riscv32"    );
+    ZigArchitectureTarget RISCV64     = of("riscv64"    );
+    ZigArchitectureTarget S390X       = of("s390x"      );
+    ZigArchitectureTarget SPARC       = of("sparc"      );
+    ZigArchitectureTarget SPARC64     = of("sparc64"    );
+    ZigArchitectureTarget SPIRV       = of("spirv"      );
+    ZigArchitectureTarget SPIRV32     = of("spirv32"    );
+    ZigArchitectureTarget SPIRV64     = of("spirv64"    );
+    ZigArchitectureTarget VE          = of("ve"         );
+    ZigArchitectureTarget WASM32      = of("wasm32"     );
+    ZigArchitectureTarget WASM64      = of("wasm64"     );
+    ZigArchitectureTarget X86         = of("x86"        );
+    ZigArchitectureTarget X86_64      = of("x86_64"     );
+    ZigArchitectureTarget XCORE       = of("xcore"      );
+    ZigArchitectureTarget XTENSA      = of("xtensa"     );
+    // @formatter:on
+
+    static ZigArchitectureTarget of(String arch) {
+        return new DefaultZigArchitectureTarget(arch);
+    }
 }
--- src/main/java/dev/lukebemish/ziggradle/ZigOperatingSystemTarget.java
+++ src/main/java/dev/lukebemish/ziggradle/ZigOperatingSystemTarget.java
@@ -6,9 +6,51 @@ import org.gradle.api.Named;
 import java.io.Serializable;
 
 public interface ZigOperatingSystemTarget extends Named, Serializable {
-    ZigOperatingSystemTarget LINUX = new DefaultZigOperatingSystemTarget("linux");
-    ZigOperatingSystemTarget MACOS = new DefaultZigOperatingSystemTarget("macos");
-    ZigOperatingSystemTarget WINDOWS = new DefaultZigOperatingSystemTarget("windows");
-    ZigOperatingSystemTarget SOLARIS = new DefaultZigOperatingSystemTarget("solaris");
-    ZigOperatingSystemTarget FREEBSD = new DefaultZigOperatingSystemTarget("freebsd");
+    // @formatter:off
+    ZigOperatingSystemTarget FREESTANDING = of("freestanding");
+    ZigOperatingSystemTarget OTHER        = of("other"       );
+    ZigOperatingSystemTarget CONTIKI      = of("contiki"     );
+    ZigOperatingSystemTarget ELFIAMCU     = of("elfiamcu"    );
+    ZigOperatingSystemTarget FUCHSIA      = of("fuchsia"     );
+    ZigOperatingSystemTarget HERMIT       = of("hermit"      );
+    ZigOperatingSystemTarget AIX          = of("aix"         );
+    ZigOperatingSystemTarget HAIKU        = of("haiku"       );
+    ZigOperatingSystemTarget HURD         = of("hurd"        );
+    ZigOperatingSystemTarget LINUX        = of("linux"       );
+    ZigOperatingSystemTarget PLAN9        = of("plan9"       );
+    ZigOperatingSystemTarget RTEMS        = of("rtems"       );
+    ZigOperatingSystemTarget SERENITY     = of("serenity"    );
+    ZigOperatingSystemTarget ZOS          = of("zos"         );
+    ZigOperatingSystemTarget DRAGONFLY    = of("dragonfly"   );
+    ZigOperatingSystemTarget FREEBSD      = of("freebsd"     );
+    ZigOperatingSystemTarget NETBSD       = of("netbsd"      );
+    ZigOperatingSystemTarget OPENBSD      = of("openbsd"     );
+    ZigOperatingSystemTarget DRIVERKIT    = of("driverkit"   );
+    ZigOperatingSystemTarget IOS          = of("ios"         );
+    ZigOperatingSystemTarget MACOS        = of("macos"       );
+    ZigOperatingSystemTarget TVOS         = of("tvos"        );
+    ZigOperatingSystemTarget VISIONOS     = of("visionos"    );
+    ZigOperatingSystemTarget WATCHOS      = of("watchos"     );
+    ZigOperatingSystemTarget ILLUMOS      = of("illumos"     );
+    ZigOperatingSystemTarget SOLARIS      = of("solaris"     );
+    ZigOperatingSystemTarget WINDOWS      = of("windows"     );
+    ZigOperatingSystemTarget UEFI         = of("uefi"        );
+    ZigOperatingSystemTarget PS3          = of("ps3"         );
+    ZigOperatingSystemTarget PS4          = of("ps4"         );
+    ZigOperatingSystemTarget PS5          = of("ps5"         );
+    ZigOperatingSystemTarget EMSCRIPTEN   = of("emscripten"  );
+    ZigOperatingSystemTarget WASI         = of("wasi"        );
+    ZigOperatingSystemTarget AMDHSA       = of("amdhsa"      );
+    ZigOperatingSystemTarget AMDPAL       = of("amdpal"      );
+    ZigOperatingSystemTarget CUDA         = of("cuda"        );
+    ZigOperatingSystemTarget MESA3D       = of("mesa3d"      );
+    ZigOperatingSystemTarget NVCL         = of("nvcl"        );
+    ZigOperatingSystemTarget OPENCL       = of("opencl"      );
+    ZigOperatingSystemTarget OPENGL       = of("opengl"      );
+    ZigOperatingSystemTarget VULKAN       = of("vulkan"      );
+    // @formatter:on
+
+    static ZigOperatingSystemTarget of(String os) {
+        return new DefaultZigOperatingSystemTarget(os);
+    }
 }
